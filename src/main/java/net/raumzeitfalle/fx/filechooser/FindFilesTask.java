878944d7e54400d82e7a66d1f350cfa74463154e
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2017-12-17 20:03:21 +0100

    "Streamed" and non-streamed "update" included.

--- src/main/java/net/raumzeitfalle/fx/filechooser/FindFilesTask.java
+++ src/main/java/net/raumzeitfalle/fx/filechooser/FindFilesTask.java
@@ -3,6 +3,8 @@ package net.raumzeitfalle.fx.filechooser;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.stream.Collectors;
 
 import javafx.application.Platform;
@@ -23,13 +25,16 @@ final class FindFilesTask extends Task<Void>{
     @Override
     protected Void call() throws Exception {
         
-        List<Path> candidates = Files.list(directory)
-            .parallel()
-            .filter(Files::isRegularFile)
-            .collect(Collectors.toList());
-        
-        clearAndUpdate(candidates);
-        
+        if (directory.getNameCount() > 1_000_000) {
+            List<Path> candidates = Files.list(directory)
+                    .parallel()
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+                clearAndUpdate(candidates);    
+        } else {
+            invokeAndWait(()-> pathsToUpdate.clear());
+            Files.list(directory).parallel().filter(Files::isRegularFile).forEach(this::addPathToList);
+        }
         return null;
     }
 
@@ -39,5 +44,14 @@ final class FindFilesTask extends Task<Void>{
               ()->{ pathsToUpdate.clear(); 
                     pathsToUpdate.addAll(paths);});
     }
+        
+    private void invokeAndWait(Runnable r) throws InterruptedException, ExecutionException {
+        FutureTask<?> task = new FutureTask<>(r, null);
+        Platform.runLater(task);
+        task.get();
+    }
     
+    private void addPathToList(Path p) {
+      Platform.runLater(()->pathsToUpdate.add(p));
+    }
 }
