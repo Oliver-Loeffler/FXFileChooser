2ab704fe3facb0991fac1df4cbb079a93f19fd48
Oliver-Loeffler <oliver.loeffler@posteo.de>
2017-12-18 22:33:12 +0100

    Streaming of paths worked quite well for large network shares ... one more try together with a smaller "active list" in ListView

--- src/main/java/net/raumzeitfalle/fx/filechooser/FindFilesTask.java
+++ src/main/java/net/raumzeitfalle/fx/filechooser/FindFilesTask.java
@@ -3,6 +3,8 @@ package net.raumzeitfalle.fx.filechooser;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.stream.Collectors;
 
 import javafx.application.Platform;
@@ -23,11 +25,18 @@ final class FindFilesTask extends Task<Void>{
     @Override
     protected Void call() throws Exception {
         
-        clearAndUpdate(Files.list(directory)
-                .parallel()
-                .filter(Files::isRegularFile)
-                .collect(Collectors.toList()));    
-       
+//        if (directory.getNameCount() > 1_000_000) {
+//            List<Path> candidates = Files.list(directory)
+//                    .parallel()
+//                    .filter(Files::isRegularFile)
+//                    .collect(Collectors.toList());
+//                clearAndUpdate(candidates);    
+//        } else {
+            invokeAndWait(()-> pathsToUpdate.clear());
+            
+            // in real "networked" world this worked quite good
+            Files.list(directory).parallel().filter(p->!p.toFile().isDirectory()).forEach(this::addPathToList);
+//        }
         return null;
     }
 
@@ -37,5 +46,14 @@ final class FindFilesTask extends Task<Void>{
               ()->{ pathsToUpdate.clear(); 
                     pathsToUpdate.addAll(paths);});
     }
-       
+        
+    private void invokeAndWait(Runnable r) throws InterruptedException, ExecutionException {
+        FutureTask<?> task = new FutureTask<>(r, null);
+        Platform.runLater(task);
+        task.get();
+    }
+    
+    private void addPathToList(Path p) {
+      Platform.runLater(()->pathsToUpdate.add(p));
+    }
 }
