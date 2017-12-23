b2aaf5203cd2b93d2f353a571ad26df76643a35c
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2017-12-23 11:22:09 +0100

    Added PathFilter hook to Swing version.

--- src/main/java/net/raumzeitfalle/fx/DemoJavaSwingIntegration.java
+++ src/main/java/net/raumzeitfalle/fx/DemoJavaSwingIntegration.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import net.raumzeitfalle.fx.filechooser.PathFilter;
 import net.raumzeitfalle.fx.filechooser.SwingFileChooser;
 
 public class DemoJavaSwingIntegration  {
@@ -18,7 +19,16 @@ public class DemoJavaSwingIntegration  {
         JPanel buttonHolder = new JPanel();
         
     JButton showDialog = new JButton("Show JavaFX Stage as Dialog in Swing: SwingFileChooser.class");
-    SwingFileChooser fileChooser = SwingFileChooser.create();
+    
+    PathFilter xml = PathFilter.create(".xml", p->p.getFileName().toString().toLowerCase().endsWith(".xml"));
+    PathFilter txt = PathFilter.create(".txt", p->p.getFileName().toString().toLowerCase().endsWith(".txt"));
+    
+    PathFilter xlsx = PathFilter.create(".xls or .xlsx", p-> p.getFileName().toString().toLowerCase().endsWith(".xls") 
+    		|| p.getFileName().toString().toLowerCase().endsWith(".xlsx"));
+    
+    PathFilter na0 = PathFilter.forFileExtension(".na0 (LMS binary files)", "n[a-z]\\d");
+     
+    SwingFileChooser fileChooser = SwingFileChooser.create(xml, xlsx, na0, txt);
     showDialog.addActionListener(l -> {
         int option = fileChooser.showOpenDialog(frame);
         System.out.println(option);
@@ -32,7 +42,7 @@ public class DemoJavaSwingIntegration  {
         buttonHolder.add(showDialog);
         frame.getContentPane().add(buttonHolder);
         frame.pack();
-        frame.setSize(400, 400);
+        frame.setSize(800, 200);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
