24ebfdc8ecf7aff0e9dc871438163a55b06fb030
Oliver-Loeffler <oliver.loeffler@posteo.de>
2018-03-15 09:58:50 +0100

    Typo fixed.

--- src/main/java/net/raumzeitfalle/fx/filechooser/FileChooserModel.java
+++ src/main/java/net/raumzeitfalle/fx/filechooser/FileChooserModel.java
@@ -22,7 +22,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;;
+import javafx.collections.transformation.FilteredList;
 
 final class FileChooserModel {
     
