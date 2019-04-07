bdc5b381b3dfac2beda5c008c9197b5b7d028627
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2019-04-07 15:19:31 +0200

    Formatting

--- src/main/java/net/raumzeitfalle/fx/DemoJavaSwingIntegration.java
+++ src/main/java/net/raumzeitfalle/fx/DemoJavaSwingIntegration.java
@@ -1,6 +1,5 @@
 package net.raumzeitfalle.fx;
 
-
 import java.awt.FlowLayout;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -18,83 +17,93 @@ import net.raumzeitfalle.fx.filechooser.Skin;
 import net.raumzeitfalle.fx.filechooser.SwingFileChooser;
 
 public class DemoJavaSwingIntegration implements WindowListener {
-  
-    public static void main(String[] args) {
-    		DemoJavaSwingIntegration app = new DemoJavaSwingIntegration();
-        SwingUtilities.invokeLater(app::initAndShowGui);
-    }
 
-    private final List<PathFilter> filter;
-    private SwingFileChooser fileChooser;
-    
-    DemoJavaSwingIntegration() {
-    		PathFilter all = PathFilter.acceptAllFiles("all files");
-    	 	PathFilter xml = PathFilter.forFileExtension("eXtensible Markup Language (xml)", "xml");
-    	    PathFilter txt = PathFilter.forFileExtension("TXT", "txt");
-    	    
-    	    PathFilter pdf = PathFilter.forFileExtension("PDF: Portable Document Format", "pdf");
-    	    PathFilter png = PathFilter.forFileExtension("*.png", "png");
-    	    
-    	    PathFilter svg = PathFilter.forFileExtension("Scalable Vector Graphics (*.svg)", "svg");
-    	    
-    	    PathFilter html = PathFilter.forFileExtension("*.html", "html").combine(PathFilter.forFileExtension("*.htm", "html"));
-    	    PathFilter xls = PathFilter.forFileExtension("*.xls", "xls").combine(PathFilter.forFileExtension("*.xlsx", "xlsx"));
-    	    
-    	    PathFilter htmlAndExcel = html.combine(xls).combine(png);
-    	    
-    	    filter = Arrays.asList(all,xml,txt,pdf,png,svg,html,xls,htmlAndExcel);
+	public static void main(String[] args) {
+		DemoJavaSwingIntegration app = new DemoJavaSwingIntegration();
+		SwingUtilities.invokeLater(app::initAndShowGui);
+	}
 
-    }
-    
-    private void initAndShowGui() {
-        JFrame frame = new JFrame("JavaFX Dialog in Swing");
-        JPanel buttonHolder = new JPanel(new FlowLayout());
-        
-	    JButton showDialog = new JButton("Show JavaFX Stage as Dialog in Swing: SwingFileChooser.class");
-	    this.fileChooser = SwingFileChooser.create(Skin.DEFAULT,"Choose any file:", this.filter.toArray(new PathFilter[0]));
-	    JLabel chosenFile = new JLabel("placeholder for filename to be selected.");
-	    
-	    showDialog.addActionListener(l -> {
-	        int option = fileChooser.showOpenDialog(frame);
-	        System.out.println(option);
-	        
-	        if (option == SwingFileChooser.APPROVE_OPTION) {
-	            SwingUtilities.invokeLater(()->chosenFile.setText("Selected file: " + fileChooser.getSelectedFile().toString()));
-	        }
-	        
-	    });
-        
-        buttonHolder.add(showDialog);
-        buttonHolder.add(chosenFile);
-        frame.getContentPane().add(buttonHolder);
-        frame.pack();
-        frame.setSize(800, 200);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+	private final List<PathFilter> filter;
+	private SwingFileChooser fileChooser;
+
+	DemoJavaSwingIntegration() {
+		PathFilter all = PathFilter.acceptAllFiles("all files");
+		PathFilter xml = PathFilter.forFileExtension("eXtensible Markup Language (xml)", "xml");
+		PathFilter txt = PathFilter.forFileExtension("TXT", "txt");
+
+		PathFilter pdf = PathFilter.forFileExtension("PDF: Portable Document Format", "pdf");
+		PathFilter png = PathFilter.forFileExtension("*.png", "png");
+
+		PathFilter svg = PathFilter.forFileExtension("Scalable Vector Graphics (*.svg)", "svg");
+
+		PathFilter html = PathFilter.forFileExtension("*.html", "html")
+				.combine(PathFilter.forFileExtension("*.htm", "html"));
+		PathFilter xls = PathFilter.forFileExtension("*.xls", "xls")
+				.combine(PathFilter.forFileExtension("*.xlsx", "xlsx"));
+
+		PathFilter htmlAndExcel = html.combine(xls).combine(png);
+
+		filter = Arrays.asList(all, xml, txt, pdf, png, svg, html, xls, htmlAndExcel);
+
+	}
+
+	private void initAndShowGui() {
+		JFrame frame = new JFrame("JavaFX Dialog in Swing");
+		JPanel buttonHolder = new JPanel(new FlowLayout());
+
+		JButton showDialog = new JButton("Show JavaFX Stage as Dialog in Swing: SwingFileChooser.class");
+		this.fileChooser = SwingFileChooser.create(Skin.DEFAULT, "Choose any file:",
+				this.filter.toArray(new PathFilter[0]));
+		JLabel chosenFile = new JLabel("placeholder for filename to be selected.");
+
+		showDialog.addActionListener(l -> {
+			int option = fileChooser.showOpenDialog(frame);
+			System.out.println(option);
+
+			if (option == SwingFileChooser.APPROVE_OPTION) {
+				SwingUtilities.invokeLater(
+						() -> chosenFile.setText("Selected file: " + fileChooser.getSelectedFile().toString()));
+			}
+
+		});
+
+		buttonHolder.add(showDialog);
+		buttonHolder.add(chosenFile);
+		frame.getContentPane().add(buttonHolder);
+		frame.pack();
+		frame.setSize(800, 200);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 	@Override
-	public void windowOpened(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {
+	}
 
 	@Override
-	public void windowClosing(WindowEvent e) {}
+	public void windowClosing(WindowEvent e) {
+	}
 
 	@Override
 	public void windowClosed(WindowEvent e) {
 		this.fileChooser.shutdown();
-		
+
 	}
 
 	@Override
-	public void windowIconified(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {
+	}
 
 	@Override
-	public void windowDeiconified(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {
+	}
 
 	@Override
-	public void windowActivated(WindowEvent e) {}
+	public void windowActivated(WindowEvent e) {
+	}
 
 	@Override
-	public void windowDeactivated(WindowEvent e) {}
-    
+	public void windowDeactivated(WindowEvent e) {
+	}
+
 }
