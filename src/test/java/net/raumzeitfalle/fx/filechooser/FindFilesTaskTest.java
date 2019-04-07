d1ea8cb794c23e46e0131b7a6d55b1207ae44ff9
Oliver <oliver@raumzeitfalle.de>
2019-04-07 13:07:55 +0200

    Added missing import.

--- src/test/java/net/raumzeitfalle/fx/filechooser/FindFilesTaskTest.java
+++ src/test/java/net/raumzeitfalle/fx/filechooser/FindFilesTaskTest.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.FXCollections;
