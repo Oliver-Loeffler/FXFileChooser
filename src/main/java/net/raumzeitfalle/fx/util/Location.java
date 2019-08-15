a1aef32d93185b447657d5902c021aeaf128a3a3
Oliver <oliver@raumzeitfalle.de>
2019-08-15 22:16:23 +0200

    Location now implements Comparator. Location set added to FileChooserModel.

--- src/main/java/net/raumzeitfalle/fx/util/Location.java
+++ src/main/java/net/raumzeitfalle/fx/util/Location.java
@@ -1,12 +1,17 @@
 package net.raumzeitfalle.fx.util;
 
 import java.nio.file.Path;
+import java.util.Comparator;
 
-public interface Location {
+public interface Location extends Comparator<Location> {
 	
 	String getName();
 	
 	boolean exists();
 	
 	Path getPath();
+	
+	default public int compare(Location a, Location b) {
+		return a.getName().compareToIgnoreCase(b.getName());
+	}
 }
