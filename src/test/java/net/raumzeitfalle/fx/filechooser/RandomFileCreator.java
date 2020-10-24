0dfbfd53926f1259241dea702291bf3a248ed9e2
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2020-10-24 11:49:08 +0200

    RandomFileCreator is just a helper for testing.
    No need to keep it within the library. Class deprecated.

--- src/test/java/net/raumzeitfalle/fx/filechooser/RandomFileCreator.java
+++ src/test/java/net/raumzeitfalle/fx/filechooser/RandomFileCreator.java
@@ -1,4 +1,4 @@
-package net.raumzeitfalle.util;
+package net.raumzeitfalle.fx.filechooser;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -8,10 +8,11 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Deprecated
 public final class RandomFileCreator {
 
     public static void main(String[] args){
-        Path start = Paths.get("/Users/oliver/Desktop/Many Files/");
+        Path start = Paths.get("./Many Files/");
         RandomFileCreator rfc = new RandomFileCreator(start);
         rfc.run();
     }
