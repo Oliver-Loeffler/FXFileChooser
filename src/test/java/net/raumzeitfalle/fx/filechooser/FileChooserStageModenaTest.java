1e120c023acba8c57e7dbd0ae6f8e6deea0eb376
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2020-10-24 23:56:33 +0200

    Added basic FXML load tests with TestFX.
    As TestFX has problems with FXML loaded as root nodes when it comes
    to interaction such as click/press/write, an updated loading meachnism
    is needed to make TestFX work.
    
    Also reproducible example for StackOverflow is needed.

---
+++ src/test/java/net/raumzeitfalle/fx/filechooser/FileChooserStageModenaTest.java
@@ -0,0 +1,50 @@
+package net.raumzeitfalle.fx.filechooser;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.nio.file.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import javafx.scene.Parent;
+
+class FileChooserStageModenaTest extends FileChooserStageTestBase {
+
+	@TempDir
+	public static Path emptyDirectory;
+	
+	public static Path withFiles = Paths.get("TestData/SomeFiles").toAbsolutePath();
+	
+	@Override
+	protected PathFilter getPathFilter() { return PathFilter.acceptAllFiles("all files"); }
+
+	@Override
+	protected Skin getSkin() { return Skin.MODENA; }
+
+	@Override
+	protected Path getStartDirectory() { return emptyDirectory; }
+	
+	/*
+	 * There seems to be an issue with TestFX when the FXML represents the root pane of a control.
+	 * If so, TestFX correctly approaches the positions of all nodes but interaction such as click,
+	 * write, press etc. is not working.
+	 * 
+	 * See StackOverflow: 
+	 * https://stackoverflow.com/questions/57051778/how-to-get-testfx-to-find-the-roots-fxid-in-testing-the-root-is-a-custom-cont
+	 * 
+	 * TODO:
+	 * User Kleopatra asked for a minimal reproducible example, I think this will be doable.
+	 */
+	
+	@Test
+	void viewCanBeLoaded() {
+
+		assertDoesNotThrow(()->lookup("#okButton").queryButton());
+		
+		Parent root = primaryStage.getScene().getRoot();
+		
+		captureImage(root, "ScreenshotModenaTheme.png");
+    }
+
+}
