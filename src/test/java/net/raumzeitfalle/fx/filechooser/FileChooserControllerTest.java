bc3c8c4189cf00671ec5bb7cff9b575b32c965fc
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2020-10-25 14:57:39 +0100

    Revert "Disabled filter selection test for Linux."
    This reverts commit 5dfa37c9cd456150231b5fc3810783ca11e4207f.

--- src/test/java/net/raumzeitfalle/fx/filechooser/FileChooserControllerTest.java
+++ src/test/java/net/raumzeitfalle/fx/filechooser/FileChooserControllerTest.java
@@ -252,7 +252,7 @@ class FileChooserControllerTest extends ApplicationTest {
 	}
 	
 	@Test
-	@EnabledOnOs({OS.WINDOWS})
+	@EnabledOnOs({OS.WINDOWS, OS.LINUX})
 	void that_pathfilters_from_file_type_menu_are_applied() {
 		
 		dirChooser.setDirectory(Paths.get("./TestData/SomeFiles"));
@@ -262,7 +262,6 @@ class FileChooserControllerTest extends ApplicationTest {
 		
 		MenuButton filterMenu = lookup("#fileExtensionFilter").query();
 		clickOn(filterMenu);
-		
 		clickOn("XML");
 		
 		sleep(300);
