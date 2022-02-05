e06f8055af2f304f39ddd3a86bd5d43e1f19b9c2
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2022-02-05 23:38:10 +0100

    Added logging so one can follow the tasks operation.

--- src/main/java/net/raumzeitfalle/fx/filechooser/FindFilesTask.java
+++ src/main/java/net/raumzeitfalle/fx/filechooser/FindFilesTask.java
@@ -22,7 +22,11 @@ package net.raumzeitfalle.fx.filechooser;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 
@@ -31,10 +35,13 @@ final class FindFilesTask extends Task<Integer>{
     private final ObservableList<IndexedPath> pathsToUpdate;
 
     private final Path directory;
+    
+    private final DoubleProperty duration;
 
     public FindFilesTask(Path searchFolder, ObservableList<IndexedPath> listOfPaths) {
         this.pathsToUpdate = Objects.requireNonNull(listOfPaths, "listOfPaths must not be null");
         this.directory = searchFolder;
+        this.duration = new SimpleDoubleProperty(0d);
     }
 
     /**
@@ -46,6 +53,7 @@ final class FindFilesTask extends Task<Integer>{
     @Override
     protected Integer call() throws Exception {
         Invoke.andWait(pathsToUpdate::clear);
+        long start = System.currentTimeMillis();
         if (null == directory) {
             return 0;
         }
@@ -63,6 +71,8 @@ final class FindFilesTask extends Task<Integer>{
         for (int f = 0; f < files.length; f++) {
             if (isCancelled()) {
                 updateProgress(f, files.length);
+                duration.set((System.currentTimeMillis()-start)/1E3);
+                buffer.flush();
                 break;
             }
             if (f % progressIntervall == 0) {
@@ -74,7 +84,39 @@ final class FindFilesTask extends Task<Integer>{
         }
         buffer.flush();
         updateProgress(files.length, files.length);
+        duration.set((System.currentTimeMillis()-start)/1E3);
         return files.length;
+    }
+
+    @Override
+    protected void running() {
+        super.running();
+        Logger.getLogger(FindFilesTask.class.getName())
+              .log(Level.INFO, "in {0}", directory.normalize().toAbsolutePath());
+    }
+
+    @Override
+    protected void succeeded() {
+        super.succeeded();
+        Logger.getLogger(FindFilesTask.class.getName())
+              .log(Level.INFO, "with {0} files out of {1} entries after {2} sec",
+                      new Object[]{pathsToUpdate.size(), getValue(), duration.get()});
+    }
+
+    @Override
+    protected void cancelled() {
+        super.cancelled();
+        Logger.getLogger(FindFilesTask.class.getName())
+              .log(Level.INFO, "with {0} files after {1} seconds!",
+                      new Object[]{pathsToUpdate.size(), duration.get()});
+    }
+
+    @Override
+    protected void failed() {
+        super.failed();
+        String message = String.format("after indexing %s files with an error.", pathsToUpdate.size());
+        Logger.getLogger(FindFilesTask.class.getName())
+              .log(Level.WARNING, message, getException());
     }
 
     protected int getProgressInterval(int length) {
