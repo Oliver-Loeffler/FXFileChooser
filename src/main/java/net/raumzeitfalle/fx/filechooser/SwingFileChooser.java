61ce4f90cc6cd8d58c79bafa531129c01ceadd3f
Oliver Löffler <Oliver-Loeffler@users.noreply.github.com>
2022-11-13 11:45:21 +0100

    Added JavaDoc.

--- src/main/java/net/raumzeitfalle/fx/filechooser/SwingFileChooser.java
+++ src/main/java/net/raumzeitfalle/fx/filechooser/SwingFileChooser.java
@@ -25,6 +25,7 @@ import java.awt.Frame;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -46,162 +47,245 @@ import net.raumzeitfalle.fx.filechooser.locations.Location;
  * TODO: Review usage schema  and assign proper mouse and keyboard actions
  * TODO: Rework demos and show new way to select directories
  * TODO: Add a transition for the scene change between file chooser and dir chooser.
- *  
  */
 public class SwingFileChooser extends JFXPanel implements HideableView {
 
-    private static final long serialVersionUID = -5879082370711306802L;
+	private static final long serialVersionUID = -5879082370711306802L;
 
-    /**
-     * Return value if cancel is chosen.
-     */
-    public static final int CANCEL_OPTION = 1;
+	/**
+	 * Return value if cancel is chosen.
+	 */
+	public static final int CANCEL_OPTION = 1;
 
-    /**
-     * Return value if approve (yes, ok) is chosen.
-     */
-    public static final int APPROVE_OPTION = 0;
+	/**
+	 * Return value if approve (yes, ok) is chosen.
+	 */
+	public static final int APPROVE_OPTION = 0;
 
-    static final String PROPERTIES_FILE = "swingfilechooser.properties";
-    static final String PROPERTY_USE_JAVAFX_DIRCHOOSER = "use.javafx.platform.directory.chooser";
-    
-    private static boolean useJavaFXPlatformDirectoryChooser = false; 
-    
-    static {
-        URL resource = SwingFileChooser.class.getClassLoader().getResource(PROPERTIES_FILE);
-        if (resource != null) {           
-            try (FileInputStream fis = new FileInputStream(new File(resource.toURI()))) {
-                Properties props = new Properties();
-                props.load(fis);
-                String value = props.getProperty(PROPERTY_USE_JAVAFX_DIRCHOOSER, "false");
-                useJavaFXPlatformDirectoryChooser = Boolean.parseBoolean(value);
-            } catch (Exception error) {
-                String message = String.format("Failed to read icon size from %s (via resource: %s)",
-                                        new Object[] {PROPERTIES_FILE, resource});
-                Logger.getLogger(SwingFileChooser.class.getName())
-                      .log(Level.WARNING, message, error);
-            }
-        }
-    }
-    
-    public static SwingFileChooser create(Skin skin, PathFilter... filter) {
-        return create(skin, "Choose file:", "", filter);
-    }
+	static final String PROPERTIES_FILE = "swingfilechooser.properties";
+	static final String PROPERTY_USE_JAVAFX_DIRCHOOSER = "use.javafx.platform.directory.chooser";
 
-    public static SwingFileChooser create(Skin skin, String title, PathFilter... filter) {
-        return create(skin, title, "", filter);
-    }
+	private static boolean useJavaFXPlatformDirectoryChooser = false;
 
-    public static SwingFileChooser create(Skin skin, String title, String pathToBrowse, PathFilter... filter) {
-        Path startHere = startPath(pathToBrowse);
-        SwingFileChooser fc = new SwingFileChooser(title);
+	static {
+		URL resource = SwingFileChooser.class.getClassLoader().getResource(PROPERTIES_FILE);
+		if (resource != null) {
+			try (FileInputStream fis = new FileInputStream(new File(resource.toURI()))) {
+				Properties props = new Properties();
+				props.load(fis);
+				String value = props.getProperty(PROPERTY_USE_JAVAFX_DIRCHOOSER, "false");
+				useJavaFXPlatformDirectoryChooser = Boolean.parseBoolean(value);
+			} catch (Exception error) {
+				String message = String.format("Failed to read icon size from %s (via resource: %s)",
+						new Object[] { PROPERTIES_FILE, resource });
+				Logger.getLogger(SwingFileChooser.class.getName()).log(Level.WARNING, message, error);
+			}
+		}
+	}
 
-        fc.model = FileChooserModel.startingIn(startHere);
-        for (PathFilter f : filter) {
-            fc.model.addOrRemoveFilter(f);
-        }
+	/**
+	 * Creates a new {@link FileChooser} within a Swing {@link JDialog} window.
+	 * File browsing will start in current working directory.
+	 * 
+	 * @param skin         {@link Skin} Defines the visual appearance of
+	 *                     FileChooser. Defaults to dark skin.
+	 * @param filter       {@link PathFilter} argument of variable length, accepts
+	 *                     all path filters as needed. In case no filter is
+	 *                     provided, a filter accepting all files will be used.
+	 * @return A configured {@link SwingFileChooser}.
+	 */
+	public static SwingFileChooser create(Skin skin, PathFilter... filter) {
+		return create(skin, "Choose file:", ".", filter);
+	}
 
-        // do all JavaFX work
-        Platform.runLater(() -> {
-            PathSupplier pathSupplier = configureDirectoryChooser(skin, startHere, fc);
-            FileChooser view = new FileChooser(pathSupplier, fc, fc.model, skin, FileChooserViewOption.STAGE);
-            Scene fileChooserScene = new Scene(view);
-            fc.setScene(fileChooserScene);
-        });
+	/**
+	 * Creates a new {@link FileChooser} within a Swing {@link JDialog} window.
+	 * File browsing will start in current working directory.
+	 * 
+	 * @param skin         {@link Skin} Defines the visual appearance of
+	 *                     FileChooser. Defaults to dark skin.
+	 * @param title        {@link String} title of the {@link FileChooser} view. In
+	 *                     case of having the JavaFX based {@link DirectoryChooser}
+	 *                     configured, the window titles for the directory and file
+	 *                     choosers will be identical.
+	 * @param filter       {@link PathFilter} argument of variable length, accepts
+	 *                     all path filters as needed. In case no filter is
+	 *                     provided, a filter accepting all files will be used.
+	 * @return A configured {@link SwingFileChooser}.
+	 */
+	public static SwingFileChooser create(Skin skin, String title, PathFilter... filter) {
+		return create(skin, title, ".", filter);
+	}
 
-        return fc;
-    }
+	/**
+	 * Creates a new {@link FileChooser} within a Swing {@link JDialog} window.
+	 * 
+	 * @param skin         {@link Skin} Defines the visual appearance of
+	 *                     FileChooser. Defaults to dark skin.
+	 * @param title        {@link String} title of the {@link FileChooser} view. In
+	 *                     case of having the JavaFX based {@link DirectoryChooser}
+	 *                     configured, the window titles for the directory and file
+	 *                     choosers will be identical.
+	 * @param pathToBrowse {@link Path} Location to start file browsing in. If the
+	 *                     argument is null or the denoted location does not exist,
+	 *                     the control will default to current working directory.
+	 * @param filter       {@link PathFilter} argument of variable length, accepts
+	 *                     all path filters as needed. In case no filter is
+	 *                     provided, a filter accepting all files will be used.
+	 * @return A configured {@link SwingFileChooser}.
+	 */
+	public static SwingFileChooser create(Skin skin, String title, String pathToBrowse, PathFilter... filter) {
+		return create(skin, title, title, pathToBrowse, filter);
+	}
 
-    private static PathSupplier configureDirectoryChooser(Skin skin, Path startHere, SwingFileChooser fc) {
-        PathSupplier pathSupplier = null;
-        if (useJavaFXPlatformDirectoryChooser) {
-            pathSupplier = FXDirectoryChooser.createIn(startHere, () -> fc.getScene().getWindow());
-        } else {
-            DirectoryChooser dirChooser = new DirectoryChooser(skin);
-            Scene dirChooserScene = new Scene(dirChooser);
-            pathSupplier = new PathSupplier() {
-                @Override
-                public void getUpdate(Consumer<Path> update) {
-                    fc.setTitle("Choose directory:");
-                    Scene previousScene = fc.getScene();
-                    String previousTitle = fc.title;
-                    fc.setScene(dirChooserScene);
-                    
-                    dirChooser.onSelect(() -> {
-                        Path selectedDir = dirChooser.selectedDirectoryProperty().get();
-                        if (null != selectedDir) {
-                            fc.setTitle(selectedDir.toString());
-                            update.accept(selectedDir);
-                        } else {
-                            fc.setTitle(previousTitle);
-                        }
-                        fc.setScene(previousScene);
-                    });
-                    
-                    dirChooser.onCancel(() -> fc.setScene(previousScene));
-                }
-            };
-        }
-        return pathSupplier;
-    }
+	/**
+	 * Creates a new {@link FileChooser} within a Swing {@link JDialog} window.
+	 * 
+	 * @param skin            {@link Skin} Defines the visual appearance of
+	 *                        FileChooser. Defaults to dark skin.
+	 * @param title           {@link String} title of the {@link FileChooser} view
+	 * @param dirChooserTitle {@link String} title of the {@link DirectoryChooser}
+	 *                        view when enabled. By default, the same title is used
+	 *                        as for the {@link FileChooser} view.
+	 * @param pathToBrowse    {@link Path} Location to start file browsing in. If
+	 *                        the argument is null or the denoted location does not
+	 *                        exist, the control will default to current working
+	 *                        directory.
+	 * @param filter          {@link PathFilter} argument of variable length,
+	 *                        accepts all path filters as needed. In case no filter
+	 *                        is provided, a filter accepting all files will be
+	 *                        used.
+	 * @return A configured {@link SwingFileChooser}.
+	 */
+	public static SwingFileChooser create(Skin skin, String title, String dirChooserTitle, String pathToBrowse,
+			PathFilter... filter) {
 
-    private static Path startPath(String pathToBrowse) {
-        Path startHere = Paths.get(pathToBrowse);
-        if (pathToBrowse.equals("")) {
-            startHere = Paths.get("./");
-        }
-        return startHere;
-    }
+		String fcTitle = (title == null) ? "Choose file:" : title;
+		String dcTitle = (dirChooserTitle == null) ? fcTitle : dirChooserTitle;
+		Skin skinToUse = (skin == null) ? Skin.DARK : skin;
+		String location = (pathToBrowse == null) ? "." : pathToBrowse;
+		Path startHere = startPath(location);
 
-    private transient FileChooserModel model;
+		PathFilter[] filters = filter;
+		if (filters.length == 0) {
+			filters = new PathFilter[] { PathFilter.acceptAllFiles("*.*") };
+		}
 
-    private JDialog dialog;
+		SwingFileChooser fc = new SwingFileChooser(fcTitle, dcTitle);
+		fc.model = FileChooserModel.startingIn(startHere);
+		for (PathFilter f : filters) {
+			fc.model.addOrRemoveFilter(f);
+		}
 
-    private String title;
+		// do all JavaFX work
+		Platform.runLater(() -> {
+			PathSupplier pathSupplier = configureDirectoryChooser(skin, startHere, fc);
+			FileChooser view = new FileChooser(pathSupplier, fc, fc.model, skinToUse, FileChooserViewOption.STAGE);
+			Scene fileChooserScene = new Scene(view);
+			fc.setScene(fileChooserScene);
+		});
 
-    private SwingFileChooser(String title) {
-        this.title = (null != title) ? title : "Choose file:";
-    }
+		return fc;
+	}
 
-    public int showOpenDialog(Component parent) {
-        if (null == this.dialog) {
-            Frame frame = JOptionPane.getFrameForComponent(parent);
-            dialog = new JDialog(frame, this.title, true);
-            dialog.setContentPane(this);
-            Dimension size = new Dimension(750, 550);
-            this.setPreferredSize(size);
-            this.setMinimumSize(size);
-            dialog.pack();
-            dialog.setResizable(true);
-        }
+	private static PathSupplier configureDirectoryChooser(Skin skin, Path startHere, SwingFileChooser fc) {
+		PathSupplier pathSupplier = null;
+		if (useJavaFXPlatformDirectoryChooser) {
+			pathSupplier = FXDirectoryChooser.createIn(startHere, () -> fc.getScene().getWindow());
+		} else {
+			DirectoryChooser dirChooser = new DirectoryChooser(skin);
+			Scene dirChooserScene = new Scene(dirChooser);
+			pathSupplier = new PathSupplier() {
+				@Override
+				public void getUpdate(Consumer<Path> update) {
+					Scene previousScene = fc.getScene();
+					String previousTitle = fc.title;
+					fc.setTitle(fc.dirChooserTitle);
+					fc.setScene(dirChooserScene);
 
-        this.dialog.setVisible(true);
-        if (this.model.invalidSelectionProperty().getValue()) {
-            return CANCEL_OPTION;
-        } else {
-            return APPROVE_OPTION;
-        }
-    }
+					dirChooser.onSelect(() -> {
+						Path selectedDir = dirChooser.selectedDirectoryProperty().get();
+						if (null != selectedDir) {
+							fc.setTitle(selectedDir.toString());
+							update.accept(selectedDir);
+						}
+						fc.setTitle(previousTitle);
+						fc.setScene(previousScene);
+					});
 
-    public File getSelectedFile() {
-        if (null != this.model.getSelectedFile()) {
-            return this.model.getSelectedFile().toFile();
-        } else {
-            return null;
-        }
-    }
+					dirChooser.onCancel(() -> {
+						fc.setTitle(previousTitle);
+						fc.setScene(previousScene);
+					});
+				}
+			};
+		}
+		return pathSupplier;
+	}
 
-    @Override
-    public void closeView() {
-        this.dialog.setVisible(false);
-    }
+	private static Path startPath(String pathToBrowse) {
+		Path startHere = Paths.get(pathToBrowse);
+		if (pathToBrowse.equals("")) {
+			startHere = Paths.get("./");
+		}
+		if (Files.notExists(startHere)) {
+			return Paths.get("./");
+		}
+		return startHere;
+	}
 
-    public void addLocations(List<Location> locations) {
-        locations.forEach(model::addLocation);
-    }
+	private transient FileChooserModel model;
 
-    protected void setTitle(String newTitle) {
-        this.title = newTitle;
-        this.dialog.setTitle(title);
-    }
+	private JDialog dialog;
+
+	private String title;
+
+	private String dirChooserTitle;
+
+	private SwingFileChooser(String title, String dirChooserTitle) {
+		this.title = (null != title) ? title : "Choose file:";
+		this.dirChooserTitle = (null != dirChooserTitle) ? dirChooserTitle : "Choose directory:";
+	}
+
+	public int showOpenDialog(Component parent) {
+		if (null == this.dialog) {
+			Frame frame = JOptionPane.getFrameForComponent(parent);
+			dialog = new JDialog(frame, this.title, true);
+			dialog.setContentPane(this);
+			Dimension size = new Dimension(750, 550);
+			this.setPreferredSize(size);
+			this.setMinimumSize(size);
+			dialog.pack();
+			dialog.setResizable(true);
+		}
+
+		this.dialog.setVisible(true);
+		if (this.model.invalidSelectionProperty().getValue()) {
+			return CANCEL_OPTION;
+		} else {
+			return APPROVE_OPTION;
+		}
+	}
+
+	public File getSelectedFile() {
+		if (null != this.model.getSelectedFile()) {
+			return this.model.getSelectedFile().toFile();
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void closeView() {
+		this.dialog.setVisible(false);
+	}
+
+	public void addLocations(List<Location> locations) {
+		locations.forEach(model::addLocation);
+	}
+
+	protected void setTitle(String newTitle) {
+		this.title = newTitle;
+		this.dialog.setTitle(title);
+	}
 }
