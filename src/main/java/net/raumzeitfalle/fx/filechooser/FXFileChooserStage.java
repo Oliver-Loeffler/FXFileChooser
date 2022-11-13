b1736b4b8dfa96c298d905d80e4d1a8222e15f91
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2022-11-13 20:26:14 +0100

    Updated FXFileChooserStage API so that the directory chooser is configurable.

--- src/main/java/net/raumzeitfalle/fx/filechooser/FXFileChooserStage.java
+++ src/main/java/net/raumzeitfalle/fx/filechooser/FXFileChooserStage.java
@@ -67,6 +67,33 @@ public class FXFileChooserStage extends Stage implements HideableView {
      * Creates a new FileChooser placed into its own stage. The new stage will be application modal by
      * default. To change modality call the {@code initModality(...)} method.
      * <p>
+     * This instance will start browsing in current working directory accepting the provided
+     * {@link PathFilter}.
+     * 
+     * @param skin             {@link Skin} Defines the visual appearance of FileChooser. In case no
+     *                         Skin is provided, MODENA will be used.
+     * @param dirChooserOption {@link DirectoryChooserOption} Defines if an integrated (same stage)
+     *                         directory chooser is used or the standard JavaFX platform specific
+     *                         directory chooser.
+     * @param filters          {@link PathFilter} argument of variable length, accepts all path filters
+     *                         as needed. In case no filter is provided, a filter accepting all files
+     *                         will be used.
+     * 
+     * @return {@link FXFileChooserStage} A file chooser in its own window.
+     */
+    public static FXFileChooserStage create(Skin skin, DirectoryChooserOption dirChooserOption, PathFilter... filters) {
+        PathFilter[] filter2use = filters;
+        if (filters.length == 0) {
+            filter2use = new PathFilter[] {PathFilter.acceptAllFiles()};
+        }
+        Skin skinToUse = skin == null ? Skin.MODENA : skin;
+        return create(skinToUse, Paths.get("."), dirChooserOption, filter2use);
+    }
+
+    /**
+     * Creates a new FileChooser placed into its own stage. The new stage will be application modal by
+     * default. To change modality call the {@code initModality(...)} method.
+     * <p>
      * This instance will start browsing in the provided directory accepting files matching the provided
      * {@link PathFilter}.
      * 
@@ -87,6 +114,36 @@ public class FXFileChooserStage extends Stage implements HideableView {
         Skin skinToUse = skin == null ? Skin.MODENA : skin;
         Path location = startsIn == null ? Paths.get(".") : startsIn;
         return new FXFileChooserStage(FileChooserModel.startingIn(location, filter2use), skinToUse);
+    }
+
+    /**
+     * Creates a new FileChooser placed into its own stage. The new stage will be application modal by
+     * default. To change modality call the {@code initModality(...)} method.
+     * <p>
+     * This instance will start browsing in the provided directory accepting files matching the provided
+     * {@link PathFilter}.
+     * 
+     * @param skin             {@link Skin} Defines the visual appearance of FileChooser. In case no
+     *                         Skin is provided, MODENA will be used.
+     * @param startsIn         {@link Path} Location where file browsing starts. If not provided, the
+     *                         file chooser will start browsing in the current working directory.
+     * @param dirChooserOption {@link DirectoryChooserOption} Defines if an integrated (same stage)
+     *                         directory chooser is used or the standard JavaFX platform specific
+     *                         directory chooser.
+     * @param filters          {@link PathFilter} argument of variable length, accepts all path filters
+     *                         as needed. In case no filter is provided, a filter accepting all files
+     *                         will be used.
+     * 
+     * @return {@link FXFileChooserStage} A file chooser in its own window.
+     */
+    public static FXFileChooserStage create(Skin skin, Path startsIn, DirectoryChooserOption dirChooserOption, PathFilter... filters) {
+        PathFilter[] filter2use = filters;
+        if (filters.length == 0) {
+            filter2use = new PathFilter[]{PathFilter.acceptAllFiles()};
+        }
+        Skin skinToUse = skin == null ? Skin.MODENA : skin;
+        Path location = startsIn == null ? Paths.get(".") : startsIn;
+        return new FXFileChooserStage(FileChooserModel.startingIn(location, filter2use), skinToUse, dirChooserOption);
     }
 
     private final FileChooserModel model;
